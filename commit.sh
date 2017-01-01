b9712b9287a05408310bf9c5eb5063eb691a3e10
Luiz Otavio <luizotrv@gmail.com>
2017-01-01 02:13:37 -0200

    first commit react

--- commit.sh
+++ commit.sh
@@ -4,10 +4,10 @@ npm run build &&
 git commit -am "$1" &&
 git push origin master &&
 
-mv -f ./build/* ../LuizOtav.io_React/ &&
+mv -f ./build/* ../LuizOtav.io/ &&
 rm -r ./build &&
 
-cd ../LuizOtav.io_React &&
+cd ../LuizOtav.io &&
 git commit -am "React prod -> $1" &&
 git push origin master &&
 cd ../LuizOtav.io_React
