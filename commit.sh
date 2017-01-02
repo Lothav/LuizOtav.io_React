fe1efbc0630319fbb1bf0d6bcc30239b3685fcc7
Luiz Otavio <luizotrv@gmail.com>
2017-01-02 15:52:17 -0200

    findme

--- commit.sh
+++ commit.sh
@@ -3,12 +3,9 @@
 npm run build &&
 git commit -am "$1" &&
 git push origin master &&
-
 mv -ft ./build/static ../LuizOtav.io &&
 mv -f ./build/* ../LuizOtav.io &&
-
 rm -r ./build &&
-
 cd ../LuizOtav.io &&
 git commit -am "React prod -> $1" &&
 git push origin master &&
