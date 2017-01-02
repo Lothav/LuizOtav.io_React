f2368edaf97a2e6765ca193bec99359fff8214ce
Luiz Otavio <luizotrv@gmail.com>
2017-01-02 15:48:21 -0200

    findme

--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ npm run build &&
 git commit -am "$1" &&
 git push origin master &&
 
-mv -ft ./build/static ../LuizOtav.io/ &&
+mv -ft ./build/static ../LuizOtav.io &&
 mv -f ./build/* ../LuizOtav.io/ &&
 
 rm -r ./build &&
