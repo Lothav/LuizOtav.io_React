8fa27018b05b8cbcac08a70522b001db20e06267
Luiz Otavio <luizotrv@gmail.com>
2017-01-02 16:02:45 -0200

    nevermind

--- commit.sh
+++ commit.sh
@@ -3,9 +3,8 @@
 npm run build &&
 git commit -am "$1" &&
 git push origin master &&
-cp -rf ./public/* ../LuizOtav.io/ &&
 cp -rf ./build/* ../LuizOtav.io/ &&
-#rm -r ./build &&
+rm -r ./build &&
 cd ../LuizOtav.io &&
 git commit -am "React prod -> $1" &&
 git push origin master &&
