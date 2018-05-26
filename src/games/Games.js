508e1aab9450c685e4116d0979171b2e1612e1a1
Luiz0tavio <me@luizotav.io>
2018-05-26 19:45:09 -0300

    adler game added

--- src/games/Games.js
+++ src/games/Games.js
@@ -4,11 +4,18 @@ import './Games.css'
 
 import coco_vs_paint from '../assets/imgs/coco_vs_paint_gif.gif'
 import breakout from '../assets/imgs/breakout_gif.gif'
+import adler from '../assets/imgs/adler_gif.gif'
 
 class Games extends Component {
     render() {
         return (
             <div className="wrapper">
+                <div className="game-box">
+                    <a href="https://github.com/Luiz0tavio/adler">
+                        <img className="game-gif" src={adler} alt="loading..."/>
+                        Adler
+                    </a>
+                </div>
                 <div className="game-box">
                     <a href="https://github.com/Luiz0tavio/breakout">
                         <img className="game-gif" src={breakout} alt="loading..."/>
